f8f017c26b69af0401c37130c1c729599da3a354
Kazuki Minamitani <ka.minami@nifty.com>
2014-05-29 00:15:21 +0900

    example updated

--- src/example.js
+++ src/example.js
@@ -1,21 +1,40 @@
 window.onload = function() {
     document.getElementById('example01Button').onclick = function (event) {
+        $("#example01-tw").empty();
+        $("#example01-words").empty();
+
+        var twmodeEnabled = $("#example01Button-tw_mode:checked").val();
+        var sourceSentence = $("#example01-input").val().trim();
+
         var config = new JoysoundJLP.Config('YOUR_USERNAME', 'YOUR_PASSWORD');
+        if (twmodeEnabled) { config.beTweetMode() };
+
         var tagger = new JoysoundJLP.Tagger(config);
 
-        var sentence = $("#example01-input").val().trim();
+        tagger.parse(sourceSentence, function(resultSet) {
+            var sentence = resultSet.first();
 
-        tagger.parse(sentence, function(resultSet) {
-            console.log(sentence);
-            console.log(resultSet);
-            console.log(resultSet.readings());
+            $("#example01-reading").text(sentence.reading());
 
-            $("#example01-reading").text(resultSet.readings());
-            $("#example01-words").empty();
-            resultSet.first().eachWord(function(eachWord) {
-                console.log(eachWord.printString());
-                $("#example01-words").append($("<li>").text(eachWord.printString())).append($("</li>"));
+            if (twmodeEnabled) {
+                addItemToTw("publicRetweetedUser", sentence.publicRetweetedUser());
+                addItemToTw("repliedUser", sentence.repliedUser());
+                addItemToTw("mentionedUsers", sentence.mentionedUsers());
+                addItemToTw("containedUrls", sentence.containedUrls());
+                addItemToTw("hashtags", sentence.hashtags());
+            }
+
+            sentence.eachWord(function(eachWord) {
+                addItemToWords(eachWord.printString());
             });
         });
+
+        var addItemToTw = function (key, value) {
+            $("#example01-tw").append($("<li>").text(key + ': ' + value).append($("</li>")));
+        }
+
+        var addItemToWords = function (value) {
+            $("#example01-words").append($("<li>").text(value).append($("</li>")));
+        }
     };
 }
